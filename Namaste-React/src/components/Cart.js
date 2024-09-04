4e027027306ea19bb2f0dfc04ac1db3af90c1dd7
Mohit Bisht <mohitbisht0135@gmail.com>
2024-09-04 14:28:05 +0530

    redux

--- Namaste-React/src/components/Cart.js
+++ Namaste-React/src/components/Cart.js
@@ -1,13 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
+import { useDispatch } from "react-redux";
 
 const Cart = () => {
-const cartItems = useSelector((store)=> store.cart.items)
-    return <div className="m-4 p-4 text-center">
+    const cartItems = useSelector((store)=> store.cart.items)
+    const dispatch = useDispatch();
+     const  handleClearCart = () => {
+        dispatch(clearCart())
+    }
+    return (
+      <div className="m-4 p-4 text-center">
         <h1 className="text-2xl font-bold">Cart</h1>
         <div className="w-6/12 m-auto">
-            <ItemList items={cartItems}/>
+          <button
+            className="p-2 m-4 border border-black bg-black text-white px-4 rounded-lg"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+
+          <ItemList items={cartItems} />
         </div>
-    </div>
+        {cartItems.length === 0 && (
+          <h1 className="text-4xl font-bold align-bottom">
+            Your Cart is Empty. Please Go and Add something to cart
+          </h1>
+        )}
+      </div>
+    );
 }
 export default Cart;
